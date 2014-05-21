f960f4f109cd23c8309c9fb4b47aa80eb319e55c
Rasmus Eskola <fruitiex@gmail.com>
2014-05-21 16:18:17 +0300

    WIP: WebM cluster support

---
+++ static/clusteredVideo.js
@@ -0,0 +1,156 @@
+/* clusteredVideo()
+ *
+ * Given a video element, callback function which fills a given arraybuffer,
+ * and how many seconds we should buffer, this function will attach a
+ * media source to the video element, fill the source buffer with data
+ * by calling bufCallback until we have bufSeconds of buffer ahead of our
+ * playing position.
+ */
+
+var clusteredVideo = function(bufCallback, videoElement, videoMetadata, clusterConcurrency, bufMinSeconds) {
+	// tempClusters contains any clusters not yet passed on to the sourceBuffer
+	// As soon as a cluster is appended to the sourceBuffer, it is deleted from
+	// tempClusters. Clusters are stored as key/value pairs where the key is the index
+	// and value is the data.
+	//
+	// pendingCluster contains the indices of pending clusters as keys, and a
+	// boolean 'true' as its value.
+	var tempClusters = {}; var pendingClusters = {};
+
+	// sbCluster points to the cluster that we must append to the sourceBuffer next,
+	// sbCluster starts from -1 which corresponds to all data before the first cluster
+	var sbCluster = -1;
+
+	var ms = new MediaSource();
+	var sb;
+
+	var getClusterTimeout;
+
+	videoElement.src = window.URL.createObjectURL(ms);
+
+	// NOTE: if you get this too early then your .webm video probably does not
+	// contain keyframes where chrome expects them, use this program for a fix:
+	// https://github.com/acolwell/mse-tools
+	ms.addEventListener('sourceclose', function(e) {
+		console.log("MEDIA SOURCE CLOSED: " + e);
+		clearTimeout(getClusterTimeout);
+	});
+
+	videoElement.addEventListener('seeking', function(e) {
+		console.log('seek to: ' + videoElement.currentTime);
+		/*
+		clearTimeout(getClusterTimeout);
+		//tempClusters = {}; pendingClusters = {};
+		sbCluster = 0; // TODO: find the cluster closest to seeked position from webm clusters
+		getNextCluster();
+		*/
+	});
+
+	ms.addEventListener('sourceopen', function() {
+		sb = ms.addSourceBuffer('video/webm; codecs="vorbis,vp8"');
+
+		// get first clusters
+		for(var i = 0; i < clusterConcurrency; i++)
+			getNextCluster();
+
+		// call appendClusters as soon as the sourceBuffer is ready to receive more data
+		sb.addEventListener('updateend', function() {
+			appendClusters();
+		});
+	}, false);
+
+	var getNextCluster = function() {
+		// if the cluster buffer is not full, fetch more clusters.
+		if(Object.keys(pendingClusters).length < clusterConcurrency) {
+			if(videoElement.buffered.length && (videoElement.currentTime + bufMinSeconds <= videoElement.buffered.end(0))) {
+				// we already have up to bufMinSeconds of video ahead of the playback head,
+				// wait 1000ms and try again
+				clearTimeout(getClusterTimeout);
+				getClusterTimeout = setTimeout(function() {
+					var tempPendingCnt = Object.keys(pendingClusters).length;
+					for(var i = 0; i < clusterConcurrency - tempPendingCnt; i++) {
+						//console.log('deferred getNextCluster()');
+						getNextCluster();
+					}
+				}, 1000);
+			} else {
+				// fetch a cluster.
+
+				// figure out which cluster id is next
+				var currentCluster = sbCluster;
+				while(true) {
+					// already buffered the whole video?
+					if(currentCluster >= videoMetadata['clusters'].length)
+						return;
+
+					if(currentCluster in pendingClusters || currentCluster in tempClusters) {
+						currentCluster++;
+					} else {
+						pendingClusters[currentCluster] = true;
+						break;
+					}
+				}
+
+				bufCallback(currentCluster, storeCallback, failCallback);
+			}
+		} else {
+			console.log("WARNING: getCluster() called even though clusterBuffer is full! (this shouldn't happen...)");
+		}
+	};
+
+	setInterval(function() {
+		// debug print
+		if(videoElement.buffered.length)
+			$("#stats").text("video played/buffered: " + Math.round(videoElement.currentTime) + 's/' + Math.round(videoElement.buffered.end(0)) + 's');
+	}, 1000);
+
+	/* Cluster handling */
+
+	// attempt appending cluster at sbCluster
+	// NOTE: this function is called when the sb event 'updateend' fires, that is,
+	// when the sb has finished appending to its buffer. This means that this
+	// function will 'loop' asynchronously whenever the sb is ready,
+	// incrementing sbCluster each iteration, until we find the first cluster that
+	// has not been fetched yet
+	var appendClusters = function() {
+		if(!(sbCluster in tempClusters))
+			return; // we are missing a cluster and can't continue
+
+		// if the sb is already processing buffers we don't need to do anything here;
+		// appendClusters(); will be called asynchronously when the sb has finished
+		if(!sb.updating) {
+			sb.appendBuffer(tempClusters[sbCluster]);
+
+			delete(tempClusters[sbCluster]);
+
+			sbCluster++;
+
+			if(videoElement.paused)
+				videoElement.play();
+		}
+	};
+
+	// called whenever a new cluster has been successfully fetched to store it
+	var storeCallback = function(currentCluster, buf) {
+		// store the cluster
+		tempClusters[currentCluster] = buf;
+
+		// no longer a pending cluster
+		delete(pendingClusters[currentCluster]);
+
+		// try appending fetched clusters into sb
+		appendClusters();
+
+		// start fetching another cluster
+		getNextCluster();
+	};
+
+	// called whenever a cluster fetch failed
+	var failCallback = function(currentCluster) {
+		// no longer a pending cluster
+		delete(pendingClusters[currentCluster]);
+
+		// start fetching another cluster (possibly this failed one)
+		getNextCluster();
+	};
+};
--- static/readVideoFile.js
+++ static/readVideoFile.js
@@ -1,32 +1,54 @@
-var byteSize = 623568;
-var chunkSize = 1024 * 64;
-var chunkBufferSize = 10; // how many chunks are fetched concurrently
-var bufMaxSeconds = 10; // try to keep at least this many seconds buffered
+var clusterConcurrency = 10; // how many clusters are fetched concurrently
+var bufMinSeconds = 10; // try to keep at least this many seconds buffered
+var url = 'http://localhost/output.webm';
+var url_clusters = 'http://localhost/output.webm.json';
+var videoElement = document.querySelector('video');
 
-var video = document.querySelector('video');
-
-var readFile = function(currentChunk, storeCallback, failCallback) {
-	var url = 'http://fruitiex.org/files/misc/output.webm';
-	var xhr = new XMLHttpRequest();
-	xhr.open('GET', url, true);
-	xhr.responseType = 'arraybuffer';
-	xhr.setRequestHeader('Range', 'bytes=' +
-			(currentChunk * chunkSize) + '-' + ((currentChunk + 1) * chunkSize));
-	xhr.send();
-	console.log('xhr.send()');
-
-	xhr.onload = function(e) {
-		if (xhr.status != 200) {
-			alert("Unexpected status code " + xhr.status + " for " + url);
-			failCallback(currentChunk);
+$.ajax(url_clusters).done(function(data) {
+	var videoMetadata = JSON.parse(data);
+	var getClusterEnd = function(currentCluster) {
+		if(currentCluster < videoMetadata['clusters'].length - 1) {
+			return videoMetadata['clusters'][currentCluster + 1].offset - 1;
 		} else {
-			storeCallback(currentChunk, new Uint8Array(xhr.response));
+			return videoMetadata['total_size'];
 		}
 	};
-}
 
-$("#startButton").click(function(e) {
-	var cnt = Math.ceil(byteSize / chunkSize);
-	chunkedVideo(video, readFile, cnt, chunkBufferSize, bufMaxSeconds);
+	var xhrRequest = function(currentCluster, storeCallback, failCallback) {
+		var xhr = new XMLHttpRequest();
+		xhr.open('GET', url, true);
+		xhr.responseType = 'arraybuffer';
+
+		// cluster -1 contains all data before the first webm cluster
+		if(currentCluster === -1) {
+			xhr.setRequestHeader('Range', 'bytes=0-' + getClusterEnd(-1));
+		} else {
+			xhr.setRequestHeader('Range', 'bytes=' +
+					videoMetadata['clusters'][currentCluster].offset + '-' +
+					getClusterEnd(currentCluster));
+		}
+
+		/*
+		console.log('getting range: ' +
+			videoMetadata['clusters'][currentCluster].offset + '-' +
+			getClusterEnd(currentCluster));
+			*/
+		xhr.send();
+
+		xhr.onload = function(e) {
+			if (xhr.status != 206 && xhr.status != 200) {
+				alert("Unexpected status code " + xhr.status + " for " + url);
+				failCallback(currentCluster);
+			} else {
+				storeCallback(currentCluster, new Uint8Array(xhr.response));
+			}
+		};
+	}
+
+	// enable the start button
+	$("#startButton").removeAttr("disabled");
+	$("#startButton").click(function(e) {
+		clusteredVideo(xhrRequest, videoElement, videoMetadata, clusterConcurrency, bufMinSeconds);
+	});
 });
 
